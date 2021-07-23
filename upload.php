da924e60817afc3bc580c78f894f332f93bf506d
Furkan Erkul <dev.furkanerkul@hotmail.com>
2021-07-23 19:32:08 +0300

    Project Files
    Upload project files

---
+++ upload.php
@@ -0,0 +1,11 @@
+<?php
+
+if (isset($_POST['image'])) {
+    $data = $_POST['image'];
+    $image_array_1 = explode(";", $data);
+    $image_array_2 = explode(",", $image_array_1[1]);
+    $data = base64_decode($image_array_2[1]);
+    $image_name = 'upload/' . time() . '.png';
+    file_put_contents($image_name, $data);
+    echo "Crop successful... <a href='$image_name' target='_blank'>Cropped Image</a>";
+}
